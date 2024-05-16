bc97e634662f3a62eb738c94894701de86e4f816
OzzyOsbourne <andreeamanole2020@gmail.com>
2024-05-16 15:41:46 +0300

    Attempt spotify like formatting of song component

--- spotify-top-songs/src/components/Song/Song.js
+++ spotify-top-songs/src/components/Song/Song.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Song = ({ id, title, artist, duration }) => {
-    return (
-      <li>
-        <div>
-          <h3>{title}</h3>
-          <p>{artist}</p>
-          <p>{duration}</p>
-        </div>
-      </li>
-    );
-  };
+  return (
+    <li className="flex items-center py-3 px-4 hover:bg-hoverGray transition duration-300 ease-in-out">
+      <div className="flex-grow">
+        <h3 className="text-lg font-semibold text-teal">{title}</h3>
+        <p className="text-sm text-gray-500">{artist}</p>
+      </div>
+      <p className="text-sm text-gray-500">{duration}</p>
+    </li>
+  );
+};
+
 export default Song;
