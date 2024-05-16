b3fd0b9a1a5f3540dea02134a26063af2d9b4fea
OzzyOsbourne <andreeamanole2020@gmail.com>
2024-05-16 15:55:13 +0300

    Add playlist songs to local storage

--- spotify-top-songs/src/components/Playlist/Playlist.js
+++ spotify-top-songs/src/components/Playlist/Playlist.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import Song from '../Song/Song'; // Import the Song component from its new location
-import songsData from '../../data/songs'; // Import songs.json from its new location
+// Playlist.js
+
+import React, { useState, useEffect } from 'react';
+import Song from '../Song/Song'; 
+import songsData from '../../data/songs'; 
 
 const Playlist = () => {
+  const [playlist, setPlaylist] = useState([]);
+
+  useEffect(() => {
+    // Check if songs data exists in local storage
+    const storedSongs = localStorage.getItem('playlist');
+    if (storedSongs) {
+      setPlaylist(JSON.parse(storedSongs));
+    } else {
+      // If not, use default songs data and save it to local storage
+      setPlaylist(songsData);
+      localStorage.setItem('playlist', JSON.stringify(songsData));
+    }
+  }, []);
+
   return (
     <div className="playlist bg-lightGray text-black p-4 rounded-lg shadow-lg">
-      <h2 className="text-3xl font-bold mb-4 text-black">
-        <span className="bg-teal px-2 py-1 rounded-lg">
+      <h2 className="text-3xl font-bold mb-4">
+        <span className="bg-teal px-2 py-1 rounded-lg text-white">
           My Daily Playlist
-        </span>
+        </span>{' '}
       </h2>
       <ul>
-        {songsData.map((song, index) => (
+        {playlist.map((song, index) => (
           <Song
             key={index}
             title={song.title}
