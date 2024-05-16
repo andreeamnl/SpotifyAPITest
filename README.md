fec205889d65f8a4d005b256c18a0db9599c6ec9
OzzyOsbourne <andreeamanole2020@gmail.com>
2024-05-16 23:53:09 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SpotifyAPITest
+
+Playlistify is a simple React app that displays daily playlists (not from Spotify). Users can like/unlike songs in the playlist, toggle dark mode, and access an About page.
+
+## Usage
+
+1. Clone the repository.
+
+2. Install dependencies.
+
+3. Run the app.
+
+## Author
+
+Andreea Manole
+
+## Where can I see it?
+
+[[Test it Here!](https://andreeamnl.github.io/SpotifyAPITest/)]
--- spotify-top-songs/src/components/Song/Song.js
+++ spotify-top-songs/src/components/Song/Song.js
@@ -4,7 +4,7 @@ import Like from '../Like/Like'; // Make sure to import the Like component
 const Song = ({ id, title, artist, duration }) => {
   return (
     <li className="flex items-center py-3 px-4 hover:bg-hoverGray transition duration-300 ease-in-out">
-      <Like songId={id} />
+        <Like songId={4} />
       <div className="flex-grow">
         <h3 className="text-lg font-semibold text-teal">{title}</h3>
         <p className="text-sm text-gray-500">{artist}</p>
